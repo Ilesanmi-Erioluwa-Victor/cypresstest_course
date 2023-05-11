8da52eb59b7977e7da700603c311a3c179640aec
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-05-11 20:27:18 +0100

    Working on Task Project for testing using Cypress

---
+++ src/components/NewTask.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useRef} from 'react'
+import "./NewTask.css";
+
+const NewTask = ({onAddTask}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NewTask
+
